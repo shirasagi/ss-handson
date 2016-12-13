911ed135e6636eea332221e1eb188860867e3124
NAKANO Hideo <pinarello.marvel@gmail.com>
2016-12-13 13:27:45 +0900

    [add] template variable handler

--- app/models/concerns/cms/addon/weather.rb
+++ app/models/concerns/cms/addon/weather.rb
@@ -6,12 +6,23 @@ module Cms::Addon
     included do
       field :weather, type: String
       permit_params :weather
+
+      template_variable_handler :weather, :template_variable_handler_weather
+      template_variable_handler :weather_code, :template_variable_handler_weather_code
     end
 
     def weather_options
       [ ["晴れ", "sunny"], ["曇り", "cloudy"],
         ["雨", "rain"], ["雪", "snow"],
       ]
+    end
+
+    def template_variable_handler_weather(*args)
+      ERB::Util.html_escape(label(:weather))
+    end
+
+    def template_variable_handler_weather_code(*args)
+      ERB::Util.html_escape(weather)
     end
   end
 end
