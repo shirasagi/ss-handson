572ee618411b195c9e2067b1c6dc31af0c713286
NAKANO Hideo <nakano@web-tips.co.jp>
2018-11-30 13:21:53 +0900

    [add] practice-2 results

--- app/models/article/video_page.rb
+++ app/models/article/video_page.rb
@@ -4,9 +4,16 @@ class Article::VideoPage
   include Cms::Addon::Meta
   include Cms::Addon::Body
   include Article::Addon::Video
+  include Cms::Addon::ParentCrumb
+  include Event::Addon::Date
+  include Map::Addon::Page
+  include Cms::Addon::RelatedPage
+  include Contact::Addon::Page
+  include Cms::Addon::Tag
   include Cms::Addon::Release
   include Cms::Addon::ReleasePlan
   include Cms::Addon::GroupPermission
+  include History::Addon::Backup
 
   set_permission_name "article_video_pages"
 
