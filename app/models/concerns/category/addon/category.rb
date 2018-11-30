cff6ea22329cb35b3e7f613cb625d41477b20e3f
NAKANO Hideo <nakano@web-tips.co.jp>
2018-11-30 11:36:17 +0900

    [add] practice-2 results

--- app/models/concerns/category/addon/category.rb
+++ app/models/concerns/category/addon/category.rb
@@ -2,5 +2,11 @@ module Category::Addon
   module Category
     extend SS::Addon
     extend ActiveSupport::Concern
+
+    included do
+      field :category_upper_html, type: String
+      field :category_lower_html, type: String
+      permit_params :category_upper_html, :category_lower_html
+    end
   end
 end
